438a6e4f5a1cc9e3e76bcd05400df080c9d047e9
micromix <40323669+Id-ware@users.noreply.github.com>
2019-07-07 12:25:00 +0300

    replaced string menu[] to int i

--- device_menu_sim_sandbox_1.ino
+++ device_menu_sim_sandbox_1.ino
@@ -1,26 +1,22 @@
+//////////
 //Warning: the code is using millis(), and will freak-out after 49 days running non-stop
 //do not use beyond that time.
+//////////////////////////////
 //TODO: write a statment that fixes the "millis() 49-days bug"
-
-//Version: 07.07.2019 8:29 AM (cursor-position edition)
-//(date is better for me than random numbers..)
+//////////////////////////////
+//
+//Version: 07.07.2019 12:23 PM (cursor-position edition)
+//edit: i removed the "String menu[]" because it is really not required for counting from 0 to 9, 
+//i replaced it with much simpler "int i" .
+////
 
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-String menu[] = { "0" , "1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" };
-String inString = "";    // string to hold input
 int i = 0;
 
 const int buttonPin = 7;    // the number of the pushbutton pin
 int buttonState;
-
-///cursor-mode///
-//////////////////////////////////////
-//unsigned long cursorTime = 0;
-//int cursorPeriod = 100;
-//bool cursorON = false;
-//////////////////////////////////////
 
 //////////////////////////////////////
 unsigned long previousTime = 0;
@@ -60,61 +56,19 @@ void loop()
   ////////////////////////////////////////////////////////////////////////////
 
   unsigned long currentTime = millis();
-  
-  ///cursor-blink-mode///
-  /*
-  if (currentTime - cursorTime > cursorPeriod)
-  {
-    cursorTime = currentTime;
-    if (cursorON == false){
-      //turn cursor sign on
-      cursorON = true;
-      lcd.setCursor(charmove, 1);
-        lcd.cursor();
-    } else {
-      //turn cursor sign off
-      cursorON = false;
-        lcd.noCursor();
-    }
-  }
-  */
-      
-
+       
   if (buttonState == HIGH) {
-	  
     previousTime = currentTime;
-	  
-    //program write down to memory the time we pressed						  
-    //the button, and save it under previousTime					  
-    //(currentTime is the millis() timer)
-	  
-    flag = true;
+    flag = true;    
     
-    //i puted flag here so the code wont turn on automaticlly.
-    //otherwise it will run all the time. a safe and simple way to make it run once, 
-    //is to make the flag go 'True' on button state 'HIGH', 
-    //and then in the other function we set it back to 'False'.	  
-	      
   }
   
   if ((currentTime - previousTime > period) && (flag == true)) {
-	  
-      //currentTime is millis(); and its subtracted  
-      //from the last time we pressed the button (previousTime)
-      //then we compare it to period which is our delay (50 milisecends)
-      //if it is bigger than our delay time (in other word, if it passed that delay time), 
-      //and the flag (from button press) is ture, the function below will start.
-      //in the function we reset the flag and the time so we can use the function again.	  
-	  
     flag = false;
     previousTime = currentTime;
     lcd.setCursor(charmove, 1);
     
-    inString = menu[i];
-
-    if ((inString.toInt()) >= 9) {
-      //this is a reset function to reset the string back to zero
-      //it is enabled once the number past number 9	    	    
+    if (i >= 9) {
       i = 0;
     }
     else
@@ -124,7 +78,7 @@ void loop()
     //cursor-position off before print.
     lcd.noCursor();
     //
-    lcd.print(inString);
+    lcd.print(i);
     
     ///////////////////////////////
     previousWaitTime = currentTime;
@@ -137,21 +91,16 @@ void loop()
       previousWaitTime = currentTime;
         
         i = 0;
+        
         if (charmove >= 6){
           charmove = 0;
         } else {
       charmove++;
         }
         //cursor-position on after movement.
-	  	lcd.setCursor(charmove, 1);
+     	lcd.setCursor(charmove, 1);
         lcd.cursor();
         //
     }
 
 }
-
-// some of my own philosophy here:
-// "You can communicate with everything in this world,
-// either if its human or non-human, animal or machine.
-// the key patience and tolerance..."
-// - I.W.
